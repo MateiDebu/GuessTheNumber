32c48196da431e8e1ebb15360ce59270d69b3079
Debu Matei <102983526+MateiDebu@users.noreply.github.com>
2022-07-21 10:08:38 +0300

    Add project files.

---
+++ GuessTheNumber/GuessNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    public class GuessNumber
+    {
+        private int number;
+        public GuessNumber()
+        {
+            Random random = new Random();
+            number = random.Next(0, 100);
+        }
+        public void Guess()
+        {
+            Console.WriteLine("Insert the number which you think is correct. The number must be between 0 and 100:");
+            int guessNumber;
+
+            do
+            {
+                string str = Console.ReadLine();
+                guessNumber = int.Parse(str);
+
+                if (guessNumber < number)
+                {
+                    Console.WriteLine("The number was to small! Insert the new number: ");
+                }
+                else
+                if (guessNumber > number)
+                {
+                    Console.WriteLine("The number was to big! Insert the new number: ");
+                }
+                else
+                {
+                    Console.WriteLine("The number is correct!");
+                }
+                   
+            } while (guessNumber != number);
+
+            Console.WriteLine("The game is over!");
+        }
+    }
+}
---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number!");
+            GuessNumber guessNumber=new GuessNumber();
+            guessNumber.Guess();
+        }
+    }
+}
